f5174286426ad4fe2e54c907eb721d53e8c16037
JudjinGM <judjingm@gmail.com>
2023-02-06 17:46:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cleancodeusecase/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.cleancodeusecase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataTextView = findViewById<TextView>(R.id.dataTextView)
+        val dataEditText = findViewById<EditText>(R.id.dataEditText)
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
